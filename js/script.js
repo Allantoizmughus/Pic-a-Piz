ea7ce943bf96fe02cfbe28f140db96e8d120ba5c
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-24 18:54:48 +0300

    Add prototype in business logic of js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ function order(size, crust, topping) {
     this.size=size;
     this.crust=crust;
     this.topping=topping;
+    this.quantity=quantity;
+}
+
+order.prototype.total=function(){
+    return (this.size + this.crust + this.topping)*this.quantity;
 }
