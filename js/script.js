9a7eb4d98bf1e654b278c6cfeec2420e8d67226b
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-04-26 03:38:57 +0300

    Modify the js command

--- js/script.js
+++ js/script.js
@@ -1,67 +1,80 @@
-//Business logic
-function Order(){
-    this.sizes=[];
-    this.crusts=[];
-    this.toppings=[];
-    this.quantity=[];
-    this.total;
-}
-
-Order.prototype.total=function(){
-    var sz=0;
-    this.sizes.map(function(size){
-         sz += size
+$(document).ready(function() {
+    $("#order-details").hide();
+    $("#deliver").hide();
+    // Business Logic
+    var totalPriceArray = [];
+    function Order(size, crust, toppings, quantity) {
+      this.sizes = size;
+      this.crusts = crust;
+      this.toppings = toppings;
+      this.pizzaPrice = 0;
+      this.quantity = quantity;
+    }
+    Order.prototype.total = function() {
+      if (this.sizes === "400") {
+        this.pizzaPrice += 400;
+      } else if (this.sizes === "800") {
+        this.pizzaPrice += 800;
+      } else if (this.sizes === "1200") {
+        this.pizzaPrice += 1200;
+      }
+      if (this.crusts === "150") {
+        this.pizzaPrice += 150;
+      } else if (this.crusts === "200") {
+        this.pizzaPrice += 200;
+      } else if (this.crusts === "100") {
+        this.pizzaPrice += 100;
+      } 
+      if (this.toppings === "50") {
+        this.pizzaPrice += 50;
+      } else if (this.toppings === "70") {
+        this.pizzaPrice += 70;
+      } else if (this.toppings === "60") {
+        this.pizzaPrice += 60;
+      } 
+    };
+    //Business logic
+    function Address(address) {
+      this.address = address;
+      this.deliveryAddress = (address);
+    }
+    Order.prototype.totalCost = function() {
+      var cartTotalPrice = [];
+      for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement++) {
+        cartTotalPrice += totalPriceArray[arrayElement];
+      }
+      return cartTotalPrice;
+    };
+    $(".btn.check-out").click(function() {
     });
-    var cr=0;
-    this.crusts.map(function(crust){
-        cr += crust
+
+    //Userinterface Logic
+    $("form#pizza").submit(function(event) {
+      event.preventDefault();
+      var size = $("select#sizes").val();
+      var crust = $("select#crusts").val();
+      var toppings = $("select#toppings").val();
+      var pizzaDetails = (size + " - " + crust + " - " + toppings);
+      var newOrder = new Order(size, crust, toppings);
+      newOrder.total();
+      totalPriceArray.push(newOrder.pizzaPrice);
+      // $("#pizza-details").hide();
+      $("#finalcost").text(newOrder.totalCost());
+      $("#summary").append("<ul><li>" + pizzaDetails + "</li></ul>");
+      // $("#size, #crust, #toppings,").val("");
     });
-    var tp=0;
-    this.toppings.map(function(topping) {
-        tp += topping
-      });
-      var qnt=0;
-      this.quantity.map(function(quantity){
-          qnt += quantity
-      });
-
-      var total=(sz + cr + tp)* qnt
-      this.total= "Your order is:" + "Ksh" + total();
-}
-
-//user interface logic
-$(document).ready(function(){
-    $("#pizza").submit(function(event){
-        event.preventDefault();
-        var newOrder=new Order();
-
-        $("#checkout").click(function(){
-            var selectsize = parseInt($("#sizes").val());
-            if(selectsize===0){
-                $(".error").show();
-            }else if(selectsize===400 || 800 || 1200){
-            Order.sizes.push(selectsize);          
-            }
-            var selectcrust = parseInt($("#crusts").val());
-            if(selectcrust===0){
-                $(".error").show();
-            }else if(selectcrust===150 || 200 || 100){
-                Order.crusts.push(selectcrust);
-            }
-            var selecttopping = parseInt($("#toppings").val());
-            if(selecttopping===0){
-                $(".error").show();
-            }else if(selecttopping===50 || 70 || 60){
-                Order.toppings.push(selecttopping);
-            }
-            var selectquantity = parseInt($("#quantity").val()); 
-            if(selectquantity===0){
-                $(".error").show();
-            }else if(selectquantity>0){
-                Order.quantity.push(selectquantity);
-            }
-            $("#confirm").text(Order.total);
-            Order.total();
-        })
-    })
-});
+    $("#orderhere").click(function() {
+      $("#deliver").toggle();
+    });
+  
+    $("#checkout").click(function() {
+      $("#orderdetails").toggle();
+    });
+    $("form#address").submit(function(event) {
+      $(".address-form").toggle();
+      event.preventDefault();
+      var address = $("input#location").val();
+      var newAddress = new Address(address);
+      $("#delivery-message").text("Your pizza will be delivered to: " + newAddress.deliveryAddress);
+    });
+  });
